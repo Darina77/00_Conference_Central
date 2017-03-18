f945b9078628eab15e45f1f5827779327f8e3914
Darina77 <darina.pishchik@gmail.com>
2017-03-18 15:47:24 +0200

    Practice6

--- src/main/java/com/google/devrel/training/conference/domain/Profile.java
+++ src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -1,8 +1,12 @@
 package com.google.devrel.training.conference.domain;
 
+import com.google.common.collect.ImmutableList;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 // TODO indicate that this class is an Entity
@@ -11,6 +15,8 @@ public class Profile {
 	String displayName;
 	String mainEmail;
 	TeeShirtSize teeShirtSize;
+
+
 
 	// TODO indicate that the userId is to be used in the Entity's key
 	@Id String userId;
@@ -53,6 +59,33 @@ public class Profile {
 
 	public void update(String displayName, TeeShirtSize teeShirtSize) {
     	this.displayName = displayName;
-    	this.teeShirtSize = TeeShirtSize.M;
+    	this.teeShirtSize = teeShirtSize;
 	}
+
+
+
+	// List of conferences the user has registered to attend
+	private List<String> conferenceKeysToAttend = new ArrayList<>();
+
+	public List<String> getConferenceKeysToAttend() {
+		return ImmutableList.copyOf(conferenceKeysToAttend);
+	}
+
+	public void addToConferenceKeysToAttend(String conferenceKey) {
+		conferenceKeysToAttend.add(conferenceKey);
+	}
+
+	/**
+	 * Remove the conferenceId from conferenceIdsToAttend.
+	 *
+	 * @param conferenceKey a websafe String representation of the Conference Key.
+	 */
+	public void unregisterFromConference(String conferenceKey) {
+		if (conferenceKeysToAttend.contains(conferenceKey)) {
+			conferenceKeysToAttend.remove(conferenceKey);
+		} else {
+			throw new IllegalArgumentException("Invalid conferenceKey: " + conferenceKey);
+		}
+	}
+
 }
